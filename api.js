f8c3c665b4ab49e205f33f853c6ed805e3736a75
champer3 <okunolas21@gmail.com>
2022-05-30 05:06:59 -0700

    API

--- api.js
+++ api.js
@@ -1,12 +1,15 @@
+// const button = document.querySelector("button");
+// const header = document.querySelector("h1");
 
+// button.addEventListener("click", () => {
+//   fetch("https://api.adviceslip.com/advice")
+//     .then((result) => result.json())
+//     .then((data) => {
+//       header.innerText = data.slip.advice;
+//     });
+// });
 
-const button = document.querySelector("button")
-const header = document.querySelector("h1")
-
-button.addEventListener("click", () =>{
-    fetch("https://api.adviceslip.com/advice")
-.then(result => result.json())
-.then(data => {
-    header.innerText = data.slip.advice
-})
-})
+fetch("https://dog.ceo/api/breed/hound/images/random")
+  .then((result) => result.json())
+  .then((data) => console.log(data.message));
+//   .then((data) => console.log(data));
